0881625759155ef8f2605c9542e25ca45212fba7
naderaRS <nadera@retailsuccess.com>
2023-10-18 10:54:11 -0400

    asdf

--- app/src/main/java/com/example/pinchzoom/ZoomViewActivity.kt
+++ app/src/main/java/com/example/pinchzoom/ZoomViewActivity.kt
@@ -12,7 +12,9 @@ class ZoomViewActivity : ComponentActivity() {
         setContentView(R.layout.zoom_view)
         val webView: WebView = findViewById(R.id.image)
         webView.getSettings().setBuiltInZoomControls(true);
-        webView.loadUrl("https://d3gh927sycat3x.cloudfront.net/tenants/4/productimages/P2IN-BKXX-035XX/1611615309_6668_01_1.jpg@Thumbnail")
+        webView.loadUrl("lineItem.product?.image?.originalImage?.url")
+
+
     }
 
 
