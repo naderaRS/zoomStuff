95e071171f1ae29dd5e96a6fbd96c64283115f9f
naderaRS <nadera@retailsuccess.com>
2023-10-18 12:28:13 -0400

    asf

--- app/src/main/java/com/example/pinchzoom/ZoomViewActivity.kt
+++ app/src/main/java/com/example/pinchzoom/ZoomViewActivity.kt
@@ -11,8 +11,8 @@ class ZoomViewActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.zoom_view)
         val webView: WebView = findViewById(R.id.image)
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.loadUrl("lineItem.product?.image?.originalImage?.url")
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.loadUrl("https://d3gh927sycat3x.cloudfront.net/tenants/4/productimages/P2IN-BKXX-035XX/1611615309_6668_01_1.jpg@Thumbnail")
 
 
     }
